7fb0b3a61ec4d05ffeb59654cf680c5c322e6aa0
Ryan Simpson <ryzizn@gmail.com>
2025-01-02 11:36:34 -0500

    feat: navigation

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const MainNav = styled.nav`
+  position: sticky;
+  top: 0;
+  display: flex;
+  height: 4rem;
+  align-items: center;
+  background-color: ${({ theme }) => theme.background};
+  justify-content: space-between;
+  z-index: 1;
+`;
+
+const Logo = styled.a`
+  width: 20px;
+  height: 20px;
+  background-color: green;
+`;
+
+const NavItems = styled.ul`
+  list-style-type: none;
+  display: flex;
+  gap: 2rem;
+`;
+
+const NavItem = styled.li``;
+
+const NavText = styled(Link)`
+  text-decoration: none;
+
+  &:hover {
+    /* TODO: fix muted import color */
+    color: #4c4c4c;
+  }
+`;
+
+const ContactButton = styled(Link)`
+  font-size: 1rem;
+
+  &:hover {
+    color: #4c4c4c;
+  }
+`;
+
+export const NavBar = () => (
+  <MainNav>
+    {/* TODO: scroll to top */}
+    <Logo href="#landing" />
+    <NavItems>
+      <NavItem>
+        <NavText href="#about">About Me</NavText>
+      </NavItem>
+      <NavItem>
+        <NavText href="#work">Featured Work</NavText>
+      </NavItem>
+      <NavItem>
+        <NavText href="#technology">Technology</NavText>
+        {/* TODO: Open and close random accordions in an interval, stop flow when user interacts with accordions */}
+      </NavItem>
+    </NavItems>
+
+    <ContactButton href="#contact">Contact</ContactButton>
+  </MainNav>
+);
