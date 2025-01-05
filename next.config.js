f75453e9639e0612824bbd4025f4fd8787d969ec
Ryan Simpson <ryzizn@gmail.com>
2025-01-05 16:46:33 -0500

    fix: svg config

--- next.config.js
+++ next.config.js
@@ -8,5 +8,29 @@ module.exports = {
         search: ''
       }
     ]
+  },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/
+      },
+
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack']
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
   }
 };
