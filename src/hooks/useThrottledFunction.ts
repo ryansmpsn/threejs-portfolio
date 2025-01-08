76c8617bdde09209035dffc71d3e66d4d79ca81d
Ryan Simpson <ryzizn@gmail.com>
2025-01-07 22:56:59 -0500

    fix: rm unused hook

--- src/hooks/useThrottledFunction.ts
+++
@@ -1,6 +0,0 @@
-import throttle from 'lodash/throttle';
-import { useMemo } from 'react';
-
-export const useThrottledFunction = (callback, delay) => {
-  return useMemo(() => throttle(callback, delay), [callback, delay]);
-};
